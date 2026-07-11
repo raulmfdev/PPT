44726df752bbca616a1b0582531d7f54a806a14c
Raul Martinez <raulmf.dev@gmail.com>
2026-07-12 00:50:33 +0200

    añadiendo interfaz

--- script.js
+++ script.js
@@ -15,12 +15,14 @@ function getComputerChoice(){
     resultado=resultado.toLowerCase()
     return resultado
 }
-function getHumanChoice(){
+/*function getHumanChoice(){
     let eleccion=prompt("Introduzca un valor valido")
     eleccion.toLowerCase()
     return eleccion
-}
-function playRound(c=getComputerChoice(), h=getHumanChoice()){
+}*/
+function playRound(input){
+    let h=input
+    let c=getComputerChoice()
     if (c==="piedra" && h === "tijeras"){
         computerScore++
         return("has perdido, piedra gana a tijeras")
@@ -46,9 +48,9 @@ function playRound(c=getComputerChoice(), h=getHumanChoice()){
     return("empate")
 }
 function playGame(){
-    for (let i=0;i<5;i++){
+    /*for (let i=0;i<5;i++){
         console.log(playRound())
-    }
+    }*/
     if (humanScore<computerScore){
         console.log("has perdido la partida")
     }
@@ -58,4 +60,47 @@ function playGame(){
     humanScore=0
     computerScore=0
 }
-playGame()
+
+const piedra=document.createElement("button")
+const papel=document.createElement("button")
+const tijeras=document.createElement("button")
+const contador=document.createElement("div")
+const contador2=document.createElement("div")
+const ganador=document.createElement("div")
+piedra.textContent="piedra"
+papel.textContent="papel"
+tijeras.textContent="tijeras"
+
+document.body.appendChild(piedra)
+document.body.appendChild(papel)
+document.body.appendChild(tijeras)
+document.body.appendChild(contador)
+document.body.appendChild(contador2)
+document.body.appendChild(ganador)
+contador2.textContent="La puntuacion del bot es:0"
+contador.textContent="Tu puntuacion es:0"
+function manejarPuntuaciones(){
+    contador.textContent="Tu puntuacion es: " + humanScore
+    contador2.textContent="La puntuacion del bot es: " + computerScore
+    if (humanScore===5){
+        ganador.textContent="Has ganado"
+    }
+    if (computerScore===5){
+        ganador.textContent="Gano la maquina"
+    }
+}
+piedra.addEventListener("click",()=>{
+    playRound("tijeras")
+    manejarPuntuaciones()
+})
+papel.addEventListener("click",()=>{
+    playRound("tijeras")
+    manejarPuntuaciones()
+})
+tijeras.addEventListener("click",()=>{
+    playRound("tijeras")
+    manejarPuntuaciones()
+})
+
+
+
